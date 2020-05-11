f8c3212bd4098f52d1d9cf288d5c58261e7c0933
Taqi Haque <taqi_haque@hotmail.com>
2020-05-11 18:43:38 -0400

    Added particle effects to gun

---
+++ Assets/Enemy.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 50f;
+    public void TakeDamage(float amount) //will be called by Gun class
+    {
+        health -= amount;
+
+        //If health less than or equal to than 0, then dies
+        if(health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}

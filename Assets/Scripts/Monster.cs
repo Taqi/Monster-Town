03e9b8e8566450dcef835f09336b122a17523ef7
Taqi Haque <taqi_haque@hotmail.com>
2020-06-01 01:26:02 -0400

    Added idle, walking, and attacking animation of skeletons + skeleton has a 40 degree vision

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -5,20 +5,61 @@ using UnityEngine.AI;
 
 public class Monster : MonoBehaviour
 {
-    public GameObject player; //What the monster will target
+    public GameObject playerObject; //What the monster will target
+    public Transform player;
     private NavMeshAgent nav; //Component inside the Monster Object
+
+    static Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Walk towards player
-        nav.SetDestination(player.transform.position);
+        Vector3 direction = player.position - this.transform.position; 
+        float angle = Vector3.Angle(direction, this.transform.forward); //Angle between player and direction
+
+        //If player is within 20 distance of the monster
+        if(Vector3.Distance(player.position, this.transform.position) < 20 && angle <45)
+        {
+            direction.y = 0; //y direction of monster stays 0 so he doesnt fall when we get too close
+
+            //Rotation angle when monster looks at us
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+
+            //When monster is within 10 distance, set the isIdle parameter to false
+            anim.SetBool("isIdle", false);
+
+            //If line (direction magnitude) between monster and player is greater than 5. Then monster moves toward us
+            if(direction.magnitude > 5)
+            {
+                this.transform.Translate(0, 0, 0.05f); //Z direction is 0.05, which is the forward axis (blue one) of the monster
+                anim.SetBool("isWalking", true); //If within 5 distance than set to true
+                anim.SetBool("isAttacking", false);
+
+                //Walk towards player
+                //nav.SetDestination(playerObject.transform.position);
+            }
+            else
+            {
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isAttacking", true);
+            }
+
+        }
+        
+        else
+        {
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isAttacking", false);
+        }
+
         
     }
 }
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    //Controls speed of mouse
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Hide and lock cursor to center of screen
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //mouseX is movement from side to side
+        //Mouse X is preprogrammed in unity that checks for x axis changes. You can find "Mouse X" in Project settings -> Input Manager (there are 2 Mouse X and Mouse Y, one for mouse and one for joystick)
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime; //Time.deltaTime is the time that has passed since the last time the Update fucntion was called. Solve issues of different frame rates
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //That way we dont over-rotate
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0F, 0F);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
